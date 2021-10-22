aadf03936cfbf86c4ae8a2067214d6525b994c82
Nyxonn <45857590+Nyxonn@users.noreply.github.com>
2021-10-22 12:34:23 +0200

    0.0.1
    - Added DiscordRPC
    - Added VPN IP Check
    - Added +- Scintilla
    - Added Panel styles
    - Imported NuGet's

--- discordAIO/main.cs
+++ discordAIO/main.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DiscordRPC;
+using ScintillaNET;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,11 +16,20 @@ namespace discordAIO
 {
     public partial class main : Form
     {
+        private string version = "v1.0";
         private protected string email = "fmediolanek@gmail.com";
+        private protected static string dAIOPass = "ceDUsRGbwAdcjUqHWkMLgARDrkx4hNJ28WbS6fPjr7cYQ87BKJTRFcUKRbJMMxeWDTEy3k9jsLSVFFV9heDeuWYfECHDzpmgYrttCMuLZRBtuPzuSJ846YBZhCa88Qj2fAaNJuuw6fhZBZfANEbRkXvaupN8rtDQVqrvLKnKW5ESAueQ2pz4QJnTdkDdM3rapFk9mU5DNP9MGAX8zymZW2MxLfj5C4p7PkPRYBxbTyawuQR9uaJZCr4bsSDhnCh2XT5CSUzHBFcVgWS7W5W9Z7SkUe4ehtymPawpwg58mpnN36sgWVSGeFXVzbQcXjfqjcUFcR8T2gBc6Ajm9wMjhCMgMNbmCMBjHKhcLghkeGWhw5wHWNZXhnDDFxnA8U4SzqdMkfTZCeaqTBaTASzKsRmGzeBGpdA9CHkLLxtJfxwj9LQ4vafzercmLJWPWGjpyeuM7CyN7ZgRbP9J6w4wzT3bxdtQXtm8RPVq5AKA6y66pSt5rdBaGbZxUjAHD2se";
+
+        public DiscordRpcClient client;
+
         public main()
         {
             InitializeComponent();
             CheckProtection();
+
+
+        //    Scintilla();
+            DiscordRPC();
         }
 
         private void CheckProtection()
@@ -51,6 +62,46 @@ namespace discordAIO
             }
 
         }
+
+        protected void DiscordRPC()
+        {
+            client = new DiscordRpcClient("811586717433331732");
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+            {
+                Details = "Running the best virus builder in the world...",
+                State = "github.com/Nyxonn",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "aio3",
+                    LargeImageText = "Discord AIO " + version
+                }
+
+            });
+        }
+
+      /*  void Scintilla()
+        {
+            scintilla1.Lexer = Lexer.Cpp;
+            scintilla1.StyleResetDefault();
+            scintilla1.Styles[Style.Default].Font = "Consolas";
+            scintilla1.Styles[Style.Default].Size = 10;
+            scintilla1.StyleClearAll();
+            scintilla1.Styles[Style.Cpp.Default].ForeColor = System.Drawing.Color.Silver;
+            scintilla1.Styles[Style.Cpp.Comment].ForeColor = System.Drawing.Color.FromArgb(0, 128, 0);
+            scintilla1.Styles[Style.Cpp.CommentLine].ForeColor = System.Drawing.Color.FromArgb(0, 128, 0);
+            scintilla1.Styles[Style.Cpp.CommentLineDoc].ForeColor = System.Drawing.Color.FromArgb(128, 128, 128);
+            scintilla1.Styles[Style.Cpp.Number].ForeColor = System.Drawing.Color.Olive;
+            scintilla1.Styles[Style.Cpp.Word].ForeColor = System.Drawing.Color.Blue;
+            scintilla1.Styles[Style.Cpp.Word2].ForeColor = System.Drawing.Color.Blue;
+            scintilla1.Styles[Style.Cpp.String].ForeColor = System.Drawing.Color.FromArgb(163, 21, 21);
+            scintilla1.Styles[Style.Cpp.Character].ForeColor = System.Drawing.Color.FromArgb(163, 21, 21);
+            scintilla1.Styles[Style.Cpp.Verbatim].ForeColor = System.Drawing.Color.FromArgb(163, 21, 21);
+            scintilla1.Styles[Style.Cpp.StringEol].BackColor = System.Drawing.Color.Pink;
+            scintilla1.Styles[Style.Cpp.Operator].ForeColor = System.Drawing.Color.Purple;
+            scintilla1.Styles[Style.Cpp.Preprocessor].ForeColor = System.Drawing.Color.Maroon;
+            scintilla1.Margins[0].Width = 16;
+        } */
 
         private void Solid_Paint(object sender, PaintEventArgs e)
         {
